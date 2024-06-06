855072ad14a38467b6cd20922b1feb5ec44b96c2
ppurskiy <ppurskiy@gmail.com>
2024-06-06 13:56:13 -0700

    denis

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'JunMing Portfolio',
+  title: 'Denis Portfolio',
   description: 'Created by RTR',
 }
 
